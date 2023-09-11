19f935a72c07030203606d0acae3a5957c7eb3af
maulikam <mdave.5191@gmail.com>
2023-09-11 13:07:31 +0530

    basic code

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,5 +1,11 @@
 package org.acme;
 
+import org.jboss.resteasy.reactive.RestResponse;
+import org.jboss.resteasy.reactive.RestResponse.Status;
+import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
+
+import io.smallrye.common.annotation.NonBlocking;
+import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -9,8 +15,17 @@ import jakarta.ws.rs.core.MediaType;
 public class GreetingResource {
 
     @GET
+    @NonBlocking
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    public Uni<String> hello() {
+        return Uni.createFrom().item("Hello from RESTEasy Reactive")
+            .log()
+            ;
     }
+
+    @ServerExceptionMapper(IllegalArgumentException.class)
+    public RestResponse error(){
+        return RestResponse.status(Status.INTERNAL_SERVER_ERROR, "Something went wrong");
+    }
+
 }
